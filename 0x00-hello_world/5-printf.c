bbc7356e4a0cf8f885f763e756cce7897fa4c139
MaureenJame <jamemaureen99@gmail.com>
2022-04-07 15:40:30 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
-*main - entry point
-*@void:no arguments
-*Descriptio:the program starts and ends here.
-*Return:value 0to exit 
-*/
-
+ * main - entry point
+ * @void: no arguments
+ * Description: the program starts and ends here.
+ * Return: value 0 to exit
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
