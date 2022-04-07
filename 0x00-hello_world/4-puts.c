4ae9c3af48fa07974fc6294d46d8d9325aeeb7b6
MaureenJame <jamemaureen99@gmail.com>
2022-04-07 14:54:42 -0700

    Task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry points
+*@void: no arguments
+*Description: the program starts and end here.
+*Return:value 0 to exit 
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
