5c7efda5fa41c2cc0048bc0bbd92ba1e2179078b
MaureenJame <jamemaureen99@gmail.com>
2022-04-25 04:02:29 -0700

    Task9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *print_number - prints an integer.
  *only using the putchar function.
@@ -6,7 +7,9 @@
  *@n: integer to be printed.
  *
  *Return: void.
+ *
  */
+
 void print_number(int n)
 {
 	unsigned int num;
