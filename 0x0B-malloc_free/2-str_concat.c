8f9951e53b2c53d38c55bee5fb17bf160d677f84
MaureenJame <jamemaureen99@gmail.com>
2022-05-05 02:44:40 -0700

    Task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,39 +1,42 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers
- * @width: input row
- * @height: input col
- * Return: int
- */
+ *  * str_concat - function that concatenates two strings.
+ *   * @s1: input char 1
+ *    * @s2: input char 2
+ *     * Return: char
+ *      */
 
-int **alloc_grid(int width, int height)
+char *str_concat(char *s1, char *s2)
 {
-	int **arr;
-	int i, j, x, f;
+		char *cat;
+			char *nul = "";
+				unsigned int i, j, x, y;
 
-	i = j = x = 0;
-	if (width <= 0 || height <= 0)
-		return (NULL);
-	/* using typecasting (int *) to dereference values */
-	arr = malloc(height * sizeof(int *));
-	if (arr == NULL)
-		return (NULL);
-	for (i = 0; i < height; i++)
-	{
-		arr[i] = malloc(width * sizeof(int));
-		if (arr[i] == NULL)
-		{
-			for (f = 0; f < i; f++)
-				free(arr[f]);
-			free(arr);
-			return (NULL);
-		}
-	}
-	for (j = 0; j < height; j++)
-	{
-		for (x = 0; x < width; x++)
-			arr[j][x] = 0;
-	}
-	return (arr);
+					i = j = x = y = 0;
+						if (s1 == NULL)
+									s1 = nul;
+							if (s2 == NULL)
+										s2 = nul;
+								while (s1[i] != '\0')
+											i += 1;
+									while (s2[j] != '\0')
+												j += 1;
+										j += 1;
+											cat = malloc((i + j) * sizeof(*cat));
+												if (cat == NULL)
+															return (NULL);
+													while (s1[x] != '\0')
+															{
+																		cat[x] = s1[x];
+																				x += 1;
+																					}
+														while (s2[y] != '\0')
+																{
+																			cat[x] = s2[y];
+																					y += 1;
+																							x += 1;
+																								}
+															cat[x] = '\0';
+																return (cat);
 }
